ab49eca646dc1bd77bdb8c08311938801d404a2a
elvisyong <elvisyong@gmail.com>
2023-10-20 03:30:44 +0800

    feat: scaffold createv1cpi

--- programs/solana-nft-fraction/src/instructions/fractionalize_nft.rs
+++ programs/solana-nft-fraction/src/instructions/fractionalize_nft.rs
@@ -1,20 +1,45 @@
-use anchor_lang::prelude::*;
-use anchor_spl::{token::{Mint, TokenAccount, Token, self, Transfer}, associated_token};
+use anchor_lang::{prelude::*, solana_program::sysvar};
+use anchor_spl::token::{Mint, TokenAccount, Token, self, Transfer};
+use mpl_token_metadata::{instructions::CreateV1CpiBuilder, accounts::Metadata, types::TokenStandard};
 
-use crate::state::fraction_details::FractionDetails;
+use crate::{state::fraction_details::FractionDetails};
 
 pub fn fractionalize_nft_handler(
     ctx: Context<FractionalizeNft>,
-    // shares_amount: u64,
+    shares_amount: u64,
 ) -> Result<()> {
+    let metaplex_program = mpl_token_metadata::programs::MPL_TOKEN_METADATA_ID;
+
+    // TODO: Fix this 
+    let signer_seeds = [&[ctx.bumps["signer"]]];
+
+    let nft_metadata_acc = Metadata::try_from(&ctx.accounts.nft_metadata_account.to_account_info())?;
+
+    CreateV1CpiBuilder::new(metaplex_program)
+    .metadata(&ctx.accounts.fraction_token_metadata.to_account_info())
+    .mint(&ctx.accounts.token_mint.to_account_info(), true)
+    .name(format!("{} fractions", nft_metadata_acc.name))
+    .uri(nft_metadata_acc.uri) // TODO: Add uri
+    .symbol(format!("{}-fraction", nft_metadata_acc.symbol))
+    .payer(&ctx.accounts.user.to_account_info())
+    .update_authority(&ctx.accounts.fraction_account.to_account_info(), true)
+    .authority(&ctx.accounts.user.to_account_info())
+    .token_standard(TokenStandard::Fungible)
+    .print_supply(mpl_token_metadata::types::PrintSupply::Limited(shares_amount))
+    .system_program(&ctx.accounts.system_program.to_account_info())
+    .sysvar_instructions(&ctx.accounts.sysvar_instructions.to_account_info())
+    .spl_token_program(&ctx.accounts.token_program.to_account_info())
+    .seller_fee_basis_points(0) // Fee to creators of this token
+    .invoke_signed(&[&signer_seeds])?;
+
     // Transfer NFT to vault
     token::transfer(
         CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
             Transfer {
-                from: ctx.accounts.token_account.to_account_info(),
+                from: ctx.accounts.nft_account.to_account_info(),
                 to: ctx.accounts.nft_vault.to_account_info(),
-                authority: ctx.accounts.signer.to_account_info(),
+                authority: ctx.accounts.user.to_account_info(),
             }
         ),
         1
@@ -30,35 +55,52 @@ pub fn fractionalize_nft_handler(
 #[derive(Accounts)]
 pub struct FractionalizeNft<'info> {
     #[account(mut)]
-    pub signer: Signer<'info>,
+    pub user: Signer<'info>,
 
     #[account(
         init, 
         space = FractionDetails::LEN,
-        payer = signer, 
+        payer = user, 
         seeds = [
             b"fraction_account", 
+            nft_mint.key().as_ref(),
             ],
         bump
     )]
     pub fraction_account: Account<'info, FractionDetails>,
-
     #[account(
         init, 
-        payer = signer, 
-        token::mint = token_mint,
+        payer = user, 
+        token::mint = nft_mint,
         token::authority = fraction_account,
         seeds = [
             b"nft_vault", 
+            nft_mint.key().as_ref(),
             ],
         bump
     )]
     pub nft_vault: Account<'info, TokenAccount>,
+    pub nft_account: Account<'info, TokenAccount>,
+    pub nft_mint: Account<'info, Mint>,
 
-    pub token_account: Account<'info, TokenAccount>,
+    /// CHECK: Will be created by the fractionalize_nft_handler
+    pub nft_metadata_account: UncheckedAccount<'info>,
+    /// CHECK: Will be created by the fractionalize_nft_handler
+    pub fraction_token_metadata: UncheckedAccount<'info>,
+
+    /// The account will be initialized if necessary.
+    ///
+    /// Must be a signer if:
+    ///   * the token mint account does not exist.
+    ///
+    /// CHECK: account checked in CPI
+    #[account(mut)]
+    pub token_mint: Signer<'info>,
     
-    pub token_mint: Account<'info, Mint>,
-
     pub token_program: Program<'info, Token>,
+    /// CHECK: account constraints for the system program
+    #[account(address = sysvar::instructions::id())]
+    pub sysvar_instructions: UncheckedAccount<'info>,
+
     pub system_program: Program<'info, System>,
 }
--- programs/solana-nft-fraction/src/state/fraction_details.rs
+++ programs/solana-nft-fraction/src/state/fraction_details.rs
@@ -2,10 +2,12 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct FractionDetails {
+    /// The authority that can withdraw the NFT from the vault. (32)
+    pub withdraw_authority: Pubkey,
     /// The number of shares that exist for this NFT. (8)
     pub shares_amount: u64,
 }
 
 impl FractionDetails {
-    pub const LEN: usize = 8;
+    pub const LEN: usize = 32 + 8;
 }
